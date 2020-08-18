e6a8690f340a7d88d292aa5f34611b4c566d29f4
sevenmatt7 <sevenmatt7@gmail.com>
2020-08-18 10:45:07 +0800

    GET POST PUT DELETE routes done

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ app.post("/items", async(req, res) => {
         console.log(err.message);
     }
 });
+
 //get all items
 app.get("/items", async(req, res) => {
     try {
@@ -30,11 +31,49 @@ app.get("/items", async(req, res) => {
         console.log(err.message);
     }
 });
+
 //get an item
+app.get("/items/:id", async(req, res) => {
+    try {
+        const {id} = req.params;
+        const item = await pool.query("SELECT * FROM sample WHERE id = $1", [id])
+
+        res.json(item.rows[0]);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
 
 //update an item
+app.put("/items/:id", async(req, res) => {
+    try {
+        const {id} = req.params;
+        const {description} = req.body;
+        const updateItem = await pool.query(
+            "UPDATE sample SET description = $1 WHERE id = $2", 
+            [description, id]
+        );
+
+        res.json("Updated item");
+    } catch (err) {
+        console.log(err.message);
+    }
+});
 
 //delete an item
+app.delete("/items/:id", async(req, res) => {
+    try {
+        const {id} = req.params;
+        const deleteItem = await pool.query(
+            "DELETE FROM sample WHERE id = $1", 
+            [id]
+        );
+
+        res.json("Deleted item");
+    } catch (err) {
+        console.log(err.message);
+    }
+});
 
 app.listen(5000, () => {
     console.log('server has started at port 5000');
