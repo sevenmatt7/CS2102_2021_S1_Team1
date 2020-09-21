26acbe37176223a916fe6b7d615b0923cd1778ef
sevenmatt7 <sevenmatt7@gmail.com>
2020-09-22 02:07:30 +0800

    Added login functionality to page

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,5 @@
 import React, {Fragment, useState} from "react";
+import {Link} from "react-router-dom"
 
 const Register = ({setAuth}) => {
 
@@ -72,6 +73,7 @@ const Register = ({setAuth}) => {
                 onChange={e => onChange(e)}/>
                 <button className="btn btn-success btn-block">Submit</button>
             </form>
+            <Link to="/login">Login</Link>
         </Fragment>
     );
 };
