f15813662bb467253029acabb168f9f0c3e798d9
sevenmatt7 <sevenmatt7@gmail.com>
2020-09-23 02:22:50 +0800

    Fixed database code

--- server/database.sql
+++ server/database.sql
@@ -1,10 +1,3 @@
--- CREATE DATABASE sampleapp;
-
--- CREATE TABLE sample(
---     id SERIAL PRIMARY KEY,
---     description VARCHAR(255)
--- );
-
 CREATE DATABASE testdb;
 CREATE extension IF NOT EXISTS "uuid-ossp";
 
