62829763c6de988117c27ea39fa63727fdc69268
sevenmatt7 <sevenmatt7@gmail.com>
2020-10-07 14:30:05 +0800

    Changed database.sql in server folder to fit schema

--- server/database.sql
+++ server/database.sql
@@ -1,120 +1,98 @@
-CREATE DATABASE testdb;
-CREATE extension IF NOT EXISTS "uuid-ossp";
-
--- CREATE TABLE Users (
--- 	user_id uuid PRIMARY KEY 
---     DEFAULT uuid_generate_v4(),
--- 	full_name VARCHAR NOT NULL,
--- 	email VARCHAR NOT NULL,
--- 	user_password VARCHAR NOT NULL,
--- 	profile_pic_address VARCHAR,
--- 	user_address VARCHAR
--- );
-
--- INSERT INTO users (full_name, email, user_password) VALUES
--- ('MATT', 'matt@g.com', 'test123');
-
 CREATE TABLE Users (
-	user_id uuid 
-    DEFAULT uuid_generate_v4(),
+	email VARCHAR,
 	full_name VARCHAR NOT NULL,
-	email VARCHAR NOT NULL,
 	user_password VARCHAR NOT NULL,
 	profile_pic_address VARCHAR,
 	user_address VARCHAR,
-	PRIMARY KEY (user_id)
+	PRIMARY KEY (email)
 );
 
 CREATE TABLE PetOwners (
-	owner_id uuid
-	REFERENCES Users(user_id)
+	owner_email VARCHAR
+	REFERENCES Users(email)
 	ON DELETE cascade,
-	PRIMARY KEY (owner_id)
+	PRIMARY KEY (owner_email)
 );
 
 CREATE TABLE Caretakers(
-	caretaker_id uuid
-	REFERENCES Users(user_id)
+	caretaker_email VARCHAR
+	REFERENCES Users(email)
 	ON DELETE cascade,
-	employment_type VARCHAR,
+	employment_type VARCHAR NOT NULL,
 	avg_rating NUMERIC,
 	no_of_reviews INTEGER,
-	PRIMARY KEY (caretaker_id)
+	PRIMARY KEY (caretaker_email)
 );
 
 CREATE TABLE PCSAdmins (
-	admin_id uuid
-	REFERENCES Users(user_id)
+	admin_email VARCHAR
+	REFERENCES Users(email)
 	ON DELETE cascade,
-	PRIMARY KEY (admin_id)
+	PRIMARY KEY (admin_email)
 );
 
 CREATE TABLE Manages (
-	admin_id uuid REFERENCES PCSAdmins(admin_id),
-	caretaker_id uuid REFERENCES Caretakers(caretaker_id),
-	PRIMARY KEY (admin_id, caretaker_id)
+	admin_email VARCHAR REFERENCES PCSAdmins(admin_email),
+	caretaker_email VARCHAR REFERENCES Caretakers(caretaker_email),
+	PRIMARY KEY (admin_email, caretaker_email)
 );
 
--- need to take note of the UNIQUE in this table, somehow it does not work without it
+CREATE TABLE Categories (
+	pet_type VARCHAR PRIMARY KEY
+);
+
 CREATE TABLE Owns_Pets (
-	owner_id uuid UNIQUE REFERENCES PetOwners(owner_id)
-	ON DELETE cascade ,
-	pet_id uuid UNIQUE,
-	gender CHAR,
-	pet_name VARCHAR,
+	owner_email VARCHAR REFERENCES PetOwners(owner_email)
+	ON DELETE cascade,
+	pet_id SERIAL,
+	gender CHAR NOT NULL,
+	pet_name VARCHAR NOT NULL,
 	special_req VARCHAR,
-	pet_type VARCHAR,
-	display_pic_address VARCHAR,
+	pet_type VARCHAR REFERENCES Categories(pet_type),
 	PRIMARY KEY (pet_id)
 );
 
-CREATE TABLE Belongs_to (
-	pet_id uuid REFERENCES Owns_Pets(pet_id),
-	breed_name VARCHAR,
-	PRIMARY KEY (pet_id)
-);
-
-CREATE TABLE Breeds (
-	breed_name VARCHAR PRIMARY KEY
-);
+-- CREATE TABLE Belongs_to (
+-- 	pet_id INTEGER REFERENCES Owns_Pets(pet_id),
+-- 	breed_name VARCHAR,
+-- 	PRIMARY KEY (pet_id)
+-- );
 
 CREATE TABLE Owns_aggregate (
-	owner_id uuid REFERENCES Owns_Pets(owner_id), 
-	pet_id uuid REFERENCES Owns_Pets(pet_id),
-	PRIMARY KEY (owner_id, pet_id)
-	
+	owner_email VARCHAR REFERENCES PetOwners(owner_email),
+	pet_id INTEGER REFERENCES Owns_Pets(pet_id),
+	PRIMARY KEY (owner_email, pet_id)
 );
 
 CREATE TABLE Offers_Services (  
-	caretaker_id uuid REFERENCES Caretakers(caretaker_id)
+	caretaker_email VARCHAR REFERENCES Caretakers(caretaker_email)
 	ON DELETE cascade,
 	service_type VARCHAR NOT NULL,
-	service_avail VARCHAR,
-	type_pref VARCHAR,
-	daily_price NUMERIC,
-	PRIMARY KEY (caretaker_id, service_type)
+	service_avail VARCHAR NOT NULL,
+	type_pref VARCHAR NOT NULL,
+	daily_price NUMERIC NOT NULL,
+	PRIMARY KEY (caretaker_email, service_type)
 );
 
 CREATE TABLE Transactions_Details (
-	caretaker_id uuid,
+	caretaker_email VARCHAR,
 	tx_type VARCHAR,
-	pet_id uuid,
-	owner_id uuid,
-	tx_id uuid DEFAULT uuid_generate_v4(),
+	pet_id INTEGER,
+	owner_email VARCHAR,
+	tx_id SERIAL,
 	owner_review VARCHAR,
 	owner_rating VARCHAR,
-	payment_mode VARCHAR,
-	cost NUMERIC,
-	mode_of_transfer VARCHAR,
-	duration INTEGER,
+	payment_mode VARCHAR NOT NULL,
+	cost NUMERIC NOT NULL,
+	mode_of_transfer VARCHAR NOT NULL,
+	duration INTEGER NOT NULL,
 	PRIMARY KEY (tx_id),
-	FOREIGN KEY (caretaker_id, tx_type) REFERENCES Offers_Services(caretaker_id, service_type),
-	FOREIGN KEY (pet_id, owner_id) REFERENCES Owns_aggregate(pet_id, owner_id)
+	FOREIGN KEY (caretaker_email, tx_type) REFERENCES Offers_services(caretaker_email, service_type),
+	FOREIGN KEY (pet_id, owner_email) REFERENCES Owns_aggregate(pet_id, owner_email)
 );
 
 CREATE TABLE Enquiries (
-	e_id uuid 
-    DEFAULT uuid_generate_v4(),
+	e_id SERIAL,
 	enq_type VARCHAR,
 	submission DATE,
 	enq_message VARCHAR,
@@ -122,13 +100,13 @@ CREATE TABLE Enquiries (
 );
 
 CREATE TABLE Enquires (
-	e_id uuid REFERENCES Enquiries(e_id),
-	user_id uuid REFERENCES Users(user_id),
-	PRIMARY KEY (e_id, user_id)
+	e_id INTEGER REFERENCES Enquiries(e_id),
+	user_email VARCHAR REFERENCES Users(email),
+	PRIMARY KEY (e_id, user_email)
 );
 
 CREATE TABLE Answers (
-	e_id uuid REFERENCES Enquiries(e_id),
-	admin_id uuid REFERENCES PCSAdmins(admin_id),
-	PRIMARY KEY (e_id, admin_id)
-);
+	e_id INTEGER REFERENCES Enquiries(e_id),
+	admin_email VARCHAR REFERENCES PCSAdmins(admin_email),
+	PRIMARY KEY (e_id, admin_email)
+); 
