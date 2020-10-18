932f80c4b3489f5aa1020f20693761cf239f0bbd
JOSHTAM <E0325893@u.nus.edu>
2020-10-18 13:51:44 +0800

    Updated database.sql tables
    -Removed Answers table and combined with Enquiries table
    -Removed service_type from Offers_Service Table
    -Changed Transaction table to add new primary key (caretaker_email, pet_name, owner_email, duration)
    -Added is_accepted & Removed tx_id

--- server/database.sql
+++ server/database.sql
@@ -55,28 +55,28 @@ CREATE TABLE Owns_Pets (
 	PRIMARY KEY (owner_email, gender, pet_name, pet_type)
 );
 
+--Removed service_type because its redundant, dont have to be specific (just take care)
 CREATE TABLE Offers_Services (  
 	caretaker_email VARCHAR REFERENCES Caretakers(caretaker_email)
 	ON DELETE cascade,
-	service_type VARCHAR NOT NULL,
-	service_avail VARCHAR NOT NULL,
+	service_avail VARCHAR NOT NULL, --Set by Caretaker
 	type_pref VARCHAR NOT NULL,
 	daily_price NUMERIC NOT NULL,
 	PRIMARY KEY (caretaker_email, service_type)
 );
 
-CREATE TABLE Petowner_Bids (  
-	owner_email VARCHAR REFERENCES PetOwners(owner_email)
-	ON DELETE cascade,
-	caretaker_email VARCHAR REFERENCES Caretakers(caretaker_email)
-	ON DELETE cascade,
-	selected_pet VARCHAR NOT NULL,
-	pet_type VARCHAR NOT NULL,
-	service_request_period VARCHAR NOT NULL,
-	offer_price NUMERIC NOT NULL,
-	transfer_mode INTEGER NOT NULL, 
-	PRIMARY KEY (owner_email, caretaker_email, selected_pet, service_request_period)
-);
+-- CREATE TABLE Petowner_Bids (  
+-- 	owner_email VARCHAR REFERENCES PetOwners(owner_email)
+-- 	ON DELETE cascade,
+-- 	caretaker_email VARCHAR REFERENCES Caretakers(caretaker_email)
+-- 	ON DELETE cascade,
+-- 	selected_pet VARCHAR NOT NULL,
+-- 	pet_type VARCHAR NOT NULL,
+-- 	service_request_period VARCHAR NOT NULL,
+-- 	offer_price NUMERIC NOT NULL,
+-- 	transfer_mode INTEGER NOT NULL, 
+-- 	PRIMARY KEY (owner_email, caretaker_email, selected_pet, service_request_period)
+-- );
 
 --Removed pet_id, changed foreign key to (owner_email, pet_name) from Owns_Pets table
 CREATE TABLE Transactions_Details (
@@ -84,35 +84,38 @@ CREATE TABLE Transactions_Details (
 	tx_type VARCHAR,
 	pet_name VARCHAR,
 	owner_email VARCHAR,
-	tx_id SERIAL,
 	owner_review VARCHAR,
 	owner_rating VARCHAR,
 	payment_mode VARCHAR NOT NULL,
 	cost NUMERIC NOT NULL,
 	mode_of_transfer VARCHAR NOT NULL,
-	duration VARCHAR NOT NULL,
-	PRIMARY KEY (tx_id),
+	duration VARCHAR NOT NULL, --Set by PetOwner
+	is_accepted BOOLEAN DEFAULT FALSE,
+	PRIMARY KEY (caretaker_email, pet_name, owner_email, duration),
 	FOREIGN KEY (caretaker_email, tx_type) REFERENCES Offers_services(caretaker_email, service_type),
 	FOREIGN KEY (owner_email, pet_name) REFERENCES Owns_Pets(owner_email, pet_name)
 );
 
 --Combined Enquires and Enquiries table, removed e_id, primary key changed to (user_email, enq_message)
-CREATE TABLE Enquires (
+--Removed Answers Table, added into Answer and Admin_email into Enquires table
+CREATE TABLE Enquiries (
 	user_email VARCHAR REFERENCES Users(email),
 	enq_type VARCHAR,
 	submission DATE,
 	enq_message VARCHAR,
+	answer VARCHAR,
+	admin_email VARCHAR REFERENCES PCSAdmins(admin_email),
 	PRIMARY KEY (user_email, enq_message)
 );
 
 --Removed e_id, changed foreign key, changed primary key
-CREATE TABLE Answers (
-	user_email VARCHAR,
-	enq_message VARCHAR,
-	admin_email VARCHAR REFERENCES PCSAdmins(admin_email),
-	FOREIGN KEY (user_email, enq_message) REFERENCES Enquires(user_email, enq_message),
-	PRIMARY KEY (user_email, enq_message, admin_email)
-); 
+-- CREATE TABLE Answers (
+-- 	user_email VARCHAR,
+-- 	enq_message VARCHAR,
+-- 	admin_email VARCHAR REFERENCES PCSAdmins(admin_email),
+-- 	FOREIGN KEY (user_email, enq_message) REFERENCES Enquires(user_email, enq_message),
+-- 	PRIMARY KEY (user_email, enq_message, admin_email)
+-- ); 
 
 -- CREATE TABLE Belongs_to (
 -- 	pet_id INTEGER REFERENCES Owns_Pets(pet_id),
